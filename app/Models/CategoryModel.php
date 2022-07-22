ba9f348dad21253418e94d6942e5f60feab2b403
caosdp-rs <caosdp@gmail.com>
2022-07-22 06:19:54 -0300

    Categories Model

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -2,41 +2,29 @@
 
 namespace App\Models;
 
-use CodeIgniter\Model;
+use App\Entities\Category;
 
-class CategoryModel extends Model
+class CategoryModel extends MyBaseModel
 {
     protected $DBGroup          = 'default';
     protected $table            = 'categories';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
+    protected $returnType       = Category::class;
+    protected $useSoftDeletes   = true;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['parent_id', 'name', 'slug'];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $beforeInsert   = ['escapeDataXSS'];
+    protected $beforeUpdate   = ['escapeDataXSS'];
 }
